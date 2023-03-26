f12f69562ec3edc6f8f636b1da87b1b55f4aa4ef
RAJASURYA2002 <cecsrajasuryar24@gmail.com>
2023-03-26 11:07:02 +0530

    commit

---
+++ script/homepage.js
@@ -0,0 +1,15 @@
+// "use strict";
+// const res=document.querySelector('.res');
+// let myMediaQuery = window.matchMedia('(min-width: 915px)');
+
+// function widthChangeCallback(myMediaQuery) {
+//   if(myMediaQuery.matches) {
+//         res.classList.add('res');
+//    } else {
+//     res.classList.remove('res');
+//    }
+// }
+
+// myMediaQuery.addEventListener('change', widthChangeCallback);
+
+// widthChangeCallback(myMediaQuery);
