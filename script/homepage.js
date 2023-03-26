3c1a92e2194e253bddc7f7e83946195d37905a45
RAJASURYA2002 <cecsrajasuryar24@gmail.com>
2023-03-26 16:16:45 +0530

    commit

--- script/homepage.js
+++ script/homepage.js
@@ -1,15 +1,16 @@
-// "use strict";
-// const res=document.querySelector('.res');
-// let myMediaQuery = window.matchMedia('(min-width: 915px)');
-
-// function widthChangeCallback(myMediaQuery) {
-//   if(myMediaQuery.matches) {
-//         res.classList.add('res');
-//    } else {
-//     res.classList.remove('res');
-//    }
-// }
-
-// myMediaQuery.addEventListener('change', widthChangeCallback);
-
-// widthChangeCallback(myMediaQuery);
+"use strict";
+const computer=document.querySelector('.display1');
+const mobile=document.querySelector('.display2');
+let myMediaQuery = window.matchMedia('(min-width: 915px)');
+function widthChangeCallback(myMediaQuery) {
+  if(myMediaQuery.matches) {
+    mobile.classList.add("display");
+    computer.classList.remove("display");
+       
+   } else {
+    mobile.classList.remove("display");
+    computer.classList.add("display");
+   }
+}
+myMediaQuery.addEventListener('change', widthChangeCallback);
+widthChangeCallback(myMediaQuery);
