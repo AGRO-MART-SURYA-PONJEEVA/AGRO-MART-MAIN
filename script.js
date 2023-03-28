9e68a3a54a15e1c9a1ef8dc4570cbc81db67b518
RAJASURYA2002 <cecsrajasuryar24@gmail.com>
2023-03-28 19:22:45 +0530

    commit

--- script.js
+++ script.js
@@ -6,11 +6,20 @@ function widthChangeCallback(myMediaQuery) {
   if(myMediaQuery.matches) {
     mobile.classList.add("display");
     computer.classList.remove("display");
-       
+    setTimeout(() => {
+      window.location.href = "/computer/html/startPageC.html";
+    }, 100);
    } else {
     mobile.classList.remove("display");
     computer.classList.add("display");
+    setTimeout(() => {
+      window.location.href = "/Mobile/html/startPageM.html";
+    }, 15000);
    }
 }
 myMediaQuery.addEventListener('change', widthChangeCallback);
 widthChangeCallback(myMediaQuery);
+// setTimeout(() => {
+  
+//   window.location.href = "/Mobile/html/startPage.html";
+// }, 15000);
