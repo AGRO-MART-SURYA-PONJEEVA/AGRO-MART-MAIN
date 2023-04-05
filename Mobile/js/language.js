90d1bb695285dce979437f7343cca49aa5e6c76e
RAJASURYA2002 <cecsrajasuryar24@gmail.com>
2023-04-05 07:12:55 +0530

    commit

--- Mobile/js/language.js
+++ Mobile/js/language.js
@@ -1,8 +1,28 @@
 "use strict";
 const languageSelector = document.querySelector(".language_selector");
+const conBtn = document.querySelector(".con");
+const heading = document.querySelector(".heading");
+const lanBoxContainer = document.querySelectorAll(".box");
+let a = 0;
 languageSelector.addEventListener("click", function (e) {
+  a++;
   const clicked = e.target.closest(".box");
   let n = clicked.dataset.lan;
   const radio = document.querySelector(`.radio_${clicked.dataset.lan}`);
   radio.click();
+  lanBoxContainer.forEach((box) => {
+    box.style.borderColor = "";
+    box.style.backgroundColor = "";
+  });
+  clicked.style.borderColor = "#2162ca";
+  clicked.style.backgroundColor = "#e8f1ff";
+  conBtn.style.backgroundColor = "#fb641b";
+});
+conBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (a > 0) {
+    languageSelector.classList.add("display");
+    heading.textContent="Agro Mart";
+    heading.style.textAlign="center";
+  }
 });
