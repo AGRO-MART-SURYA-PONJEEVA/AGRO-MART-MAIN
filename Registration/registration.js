f28230feccd3c77f496ab20eadafb79031a9e489
RAJASURYA2002 <cecsrajasuryar24@gmail.com>
2023-04-06 12:03:43 +0530

    commit

--- Registration/registration.js
+++ Registration/registration.js
@@ -7,10 +7,19 @@ const phoneInput = window.intlTelInput(phoneInputField, {
   });
 
   const nextPage=document.querySelector('.next_page');
+  const nextPage2=document.querySelector('.next_page_2');
   const formContaine=document.querySelector('.form_container');
   const formContaine2=document.querySelector('.form_container_2');
+  const formContaine3=document.querySelector('.id_display');
   nextPage.addEventListener('click',function(e)
   {
     formContaine.classList.add("display");
     formContaine2.classList.remove("display");
   })
+  nextPage2.addEventListener('click',function(e)
+  {
+    formContaine2.classList.add("display");
+    formContaine3.classList.remove("display");
+  })
+
+  //registration detail get
