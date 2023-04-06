964b739a24e41164b6d77026abb8d046aed893af
RAJASURYA2002 <cecsrajasuryar24@gmail.com>
2023-04-06 15:29:20 +0530

    commit

--- Registration/registration.js
+++ Registration/registration.js
@@ -6,20 +6,28 @@ const phoneInput = window.intlTelInput(phoneInputField, {
       "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
   });
 
-  const nextPage=document.querySelector('.next_page');
-  const nextPage2=document.querySelector('.next_page_2');
-  const formContaine=document.querySelector('.form_container');
-  const formContaine2=document.querySelector('.form_container_2');
-  const formContaine3=document.querySelector('.id_display');
-  nextPage.addEventListener('click',function(e)
-  {
-    formContaine.classList.add("display");
-    formContaine2.classList.remove("display");
-  })
-  nextPage2.addEventListener('click',function(e)
-  {
-    formContaine2.classList.add("display");
-    formContaine3.classList.remove("display");
-  })
+  //container_display//
+ const page1=document.querySelector('.form_container');
+ const page2=document.querySelector('.form_container_2');
+ const page3=document.querySelector('.id_display');
 
-  //registration detail get
+  //input select//
+  //page 1//
+  const fullName=document.querySelector('.fullname');
+  const email=document.querySelector('.email');
+  const mobile=document.querySelector('.mobile');
+  const radio=document.querySelector('.radio');
+  const password=document.querySelector('.password');
+  const conPassword=document.querySelector('.conpassword');
+
+  // page 2
+  const pincode=document.querySelector('.pincode');
+  const state=document.querySelector('.state');
+  const city=document.querySelector('.city');
+  const house=document.querySelector('.house');
+  const road=document.querySelector('.road');
+  const landmark=document.querySelector('.landmark');
+
+ //button select//
+
+ 
