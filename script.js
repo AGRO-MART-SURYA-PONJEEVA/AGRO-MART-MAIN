fa926d49a33acfa9bef8fbd63c21c3c11cbb06cb
RAJASURYA2002 <cecsrajasuryar24@gmail.com>
2023-04-09 16:22:36 +0530

    commit

--- script.js
+++ script.js
@@ -7,15 +7,28 @@ function widthChangeCallback(myMediaQuery) {
     mobile.classList.add("display");
     computer.classList.remove("display");
     setTimeout(() => {
-      window.location.href = "/computer/html/startPageC.html";
+      // window.location.href = "/computer/html/startPageC.html";
     }, 100);
    } else {
     mobile.classList.remove("display");
     computer.classList.add("display");
     setTimeout(() => {
-      window.location.href = "/Mobile/html/startPageM.html";
+      // window.location.href = "/Mobile/html/startPageM.html";
     }, 10000);
    }
 }
 myMediaQuery.addEventListener('change', widthChangeCallback);
 widthChangeCallback(myMediaQuery);
+const imgSlide=document.querySelector('.img_slide');
+let i=1;
+const tick=function()
+{
+  if(i!=5){
+ imgSlide.src=`/image/${i++}.webp`;
+  }
+  else
+  {
+    i=1;
+  }
+}
+setInterval(tick, 3000);
